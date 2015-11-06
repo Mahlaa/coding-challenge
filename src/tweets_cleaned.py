358529799f456bb294b0a89f8ec8e4df90a92155
Mahlaa <mahla@nyu.edu>
2015-11-05 23:46:18 -0500

    Update tweets_cleaned.py

--- src/tweets_cleaned.py
+++ src/tweets_cleaned.py
@@ -1,1 +1,24 @@
 # example of program that calculates the number of tweets cleaned
+import json, sys
+
+ft1 = open("ft1.txt" , 'w')
+count = 0
+
+tweets = []
+data = [{}]
+
+
+for line in open('tweets.txt'):
+  try: 
+    tweets.append(json.loads(line))
+  except:
+    pass
+
+
+for data in tweets:
+	if 'text' in data:
+		if(data['text'].encode('ascii', 'ignore') != data['text'].encode("utf8")):	
+			count += 1
+		ft1.write (data['text'].encode('ascii', 'ignore') + ' (timestamp:'+data['created_at']+')\n')
+
+ft1.write(str(count)+' tweets contained unicode.')
